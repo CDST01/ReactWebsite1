97222497c26a062b10ec91bab3c86fda111cc5c2
Cameron <cameron890@hotmail.com>
2021-05-11 17:22:58 +1000

    added article list

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import '../styles/Home.css'
 import {Banner} from '../components/Banner'
+import {useState, useEffect} from 'react'
 
 
 const BannerStyle = {
@@ -11,14 +12,50 @@ const BannerStyle = {
 }
 
 export function Home( props ) {
+// store articles
+const [articles, setArticles]= useState([])   
+   
+   
+   
+   useEffect( () => {
+      if( articles.length === 0) {
+         props.items().then( (data) => {
+            setArticles (data)
+         })
+      }
+   })
    props.items().then( (articles) => {
       console.log(articles)
    })
+
+   const ArticleView = (props) => {
+      return(
+         <div className="article">
+            <h3>{props.article.title}</h3>
+            <p>{props.article.author}</p>
+            </div>
+            )
+   }
    
+const ArticlesList = (props) => {
+   if(articles) {
+      const Collection = articles.map( (article) => {
+         return <ArticleView article={article} />
+      }) 
+   return Collection
+   }
+   else {
+      return( <p>Sorry no Articles!</p>)
+   }
+
+}
+
+
    return(
       <main className="home">
    <Banner text="Welcome" style={BannerStyle} />
    <h1>Home</h1>
+   <ArticlesList/>
    </main> 
 
    )
